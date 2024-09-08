f4b4c10a3d170fec6f871c6c33b6b87885493522
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-09-08 00:24:42 -0400

    update app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 const App = () => {
+  const name = 'User';
+
   return (
     <div className="App">
-      <h1>Hello, Buenos Dias!</h1>
+      <h1>Hello, Buenos Dias {name}!</h1>
     </div>
   );
 }
