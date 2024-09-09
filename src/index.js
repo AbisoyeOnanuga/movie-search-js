c0ab30b145a1e92e5e510412bf0c9bb1b5bf203a
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-09-09 18:59:38 -0400

    update index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDom.render(<App />, document.getELementById('root'));
+ReactDOM.render(<App />, document.getELementById('root'));
