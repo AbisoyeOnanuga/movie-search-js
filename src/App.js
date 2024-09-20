d5269750c3d571962d9ee409d19a6f675815b305
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-09-19 20:40:41 -0400

    update App css js

--- src/App.js
+++ src/App.js
@@ -12,17 +12,22 @@ const App = () => {
     const searchMovies = async (title) => {
         const options = {
             method: 'GET',
-            url: `https://imdb-movies-web-series-etc-search.p.rapidapi.com/${title}.json`,
+            url: 'https://movie-database-alternative.p.rapidapi.com/',
+            params: {
+                s: title,
+                r: 'json',
+                page: '1'
+            },
             headers: {
                 'x-rapidapi-key': 'f2bbb5b1a3msh7d83c14ad42bc40p1625f7jsn1b8d7d32e5a8',
-                'x-rapidapi-host': 'imdb-movies-web-series-etc-search.p.rapidapi.com'
+                'x-rapidapi-host': 'movie-database-alternative.p.rapidapi.com'
             }
         };
 
         try {
             const response = await axios.request(options);
             console.log(response.data); // Log the response to check its structure
-            setMovies(response.data.d); // Access the 'd' array from the response
+            setMovies(response.data.Search); // Access the 'Search' array from the response
             setLoading(false);
         } catch (error) {
             setError(error.message);
@@ -31,7 +36,7 @@ const App = () => {
     };
 
     useEffect(() => {
-        searchMovies('spiderman');
+        searchMovies('Avengers Endgame');
     }, []);
 
     useEffect(() => {
@@ -54,10 +59,9 @@ const App = () => {
             <TitleById />
             <div className="movies-grid">
                 {movies.map(movie => (
-                    <div key={movie.id} className="movie-card">
-                        <img src={movie.i.imageUrl} alt={movie.l} />
-                        <h2>{movie.l} ({movie.y})</h2>
-                        <p>{movie.s}</p>
+                    <div key={movie.imdbID} className="movie-card">
+                        <img src={movie.Poster} alt={movie.Title} />
+                        <h2>{movie.Title} ({movie.Year})</h2>
                     </div>
                 ))}
             </div>
