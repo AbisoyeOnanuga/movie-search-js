221c83057f7ec348fd2c6cd40bb2708cd89d46e9
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-11-01 19:52:52 -0400

    remove apolloClient

--- src/apolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://graph.imdbapi.dev/v1',
-  cache: new InMemoryCache()
-});
-
-export default client;
